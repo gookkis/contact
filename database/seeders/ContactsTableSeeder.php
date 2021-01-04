2917888711dfba23a467873b46b0494ff3996000
Heri Kiswanto <heryoulie@gmail.com>
2021-01-05 00:16:16 +0700

    first commit

---
+++ database/seeders/ContactsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use Illuminate\Database\Seeder;
+
+class ContactsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //factory(Contact::class, 10)->create();
+        Contact::factory()->count(10)->create();
+    }
+}
